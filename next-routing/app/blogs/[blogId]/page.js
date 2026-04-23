b5993f19e475dad412b00a1c9676ad591a9f1457
Faria Waseer <fariawaseer@gmail.com>
2026-04-23 12:52:14 +0500

    static site generation in next js

--- next-routing/app/blogs/[blogId]/page.js
+++ next-routing/app/blogs/[blogId]/page.js
@@ -1,6 +1,26 @@
 import { notFound } from "next/navigation";
 import React from "react";
+// variable for not generating SSR pages if user want to vist new page only serve those pages ehich server is generated on build Time..
+export const dynamicParams = true;
+// static site generation for page
+export async function generateStaticparams() {
+  //  data from api
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  console.log(data);
+  return data.map(({ id }) => ({
+    blogId: `${id}`,
+  }));
 
+  // fixed data
+  // return [
+  //   { blogId: "1" },
+  //   { blogId: "2" },
+  //   { blogId: "3" },
+  //   { blogId: "4" },
+  //   { blogId: "5" },
+  // ];
+}
 async function Blog({ params }) {
   const { blogId } = await params;
   if (!/^\d+$/.test(blogId)) {
